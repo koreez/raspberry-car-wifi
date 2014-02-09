eacf4f9beb8bee44a0aa59b59fa71981689b255a
koreez <zan.korez.22@gmail.com>
2014-02-09 17:05:42 +0100

    Python update

--- python/script.py
+++ python/script.py
@@ -4,11 +4,16 @@ from time import sleep
 
 def uklop():
 	gpio.setup(8, gpio.OUT)
+	puts "Turning on 8"
 	gpio.output(8,True)
 	sleep(1)
+	puts "Turning off 8"
 	gpio.output(8,False)
 	sleep(1)
 	
+	
+print "Script is starting..."
 for num in range(1,10):
 	uklop()
-	
+
+print "Done."
