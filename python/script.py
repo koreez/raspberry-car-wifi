81c3cdfa0318efd3f9489eab124f9bff615fbfc0
Jakob <jakob.boss@gmail.com>
2014-03-02 09:43:06 +0100

    Script and web updates

--- python/script.py
+++ python/script.py
@@ -4,18 +4,18 @@ from time import sleep
 
 def init(pin):
 	gpio.setup(8, gpio.OUT)
-	
+
 def turnOn(pin):
 	gpio.output(8,True)
-	
+
 def turnOff(pin):
 	gpio.output(8,False)
-	
+
 def getStatus():
 	f = open('../script/input.txt', 'r')
 	return f.read(1)
 
-	
+
 ### Main Program
 
 up = 8
@@ -35,28 +35,27 @@ print "Done."
 
 while True:
 	status = getStatus()
-	
+
 	if status == '1':
 		print "Going UP"
 		turnOn(up)
-		
+
 	elif status == '2':
 		print "Going Back"
 		turnOn(down)
-		
+
 	elif status == '3':
 		print "Going Left"
 		turnOn(left)
-		
+
 	elif status == '4':
 		print "Going Right"
 		turnOn(right)
-		
+
 	else:
-		print "Idle"
 		turnOff(up)
 		turnOff(down)
 		turnOff(left)
 		turnOff(right)
 
-	sleep(1)
+	sleep(0.1)
